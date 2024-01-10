f2a01c76667158b46e77fd53b8e7a8cd6f06b921
Konbor704 <konbor12@gmail.com>
2024-01-10 20:03:11 +0100

    neorg changes

--- nvim/lua/plugins/norg.lua
+++ nvim/lua/plugins/norg.lua
@@ -7,19 +7,27 @@ return {
     require("neorg").setup({
       load = {
         ["core.dirman.utils"] = {},
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        },
         ["core.ui"] = {},
         ["core.integrations.treesitter"] = {},
         ["core.defaults"] = {}, -- Loads default behaviour
         ["core.concealer"] = {
           config = {
-            icon_preset = "varied",
+            icon_preset = "diamond",
+            init_open_folds = "never",
           },
         },              -- Adds pretty icons to your documents
         ["core.dirman"] = { -- Manages Neorg workspaces
           config = {
             workspaces = {
+              main = "~/Neorg/",
               games = "~/Neorg/Games",
-              godot = "~/Neorg/Games/godot",
+              politics = "~/Neorg/Politics",
+              Rust = "~/Neorg/Programming/Rust"
             },
           },
         },
@@ -27,8 +35,22 @@ return {
         ["core.qol.todo_items"] = {},
         ["core.integrations.telescope"] = {},
         ["external.exec"] = {},
-        ["core.esupports.indent"] = {},
-        ["core.esupports.metagen"] = {},
+        ["core.summary"] = {},
+        ["core.pivot"] = {},
+        ["core.itero"] = {},
+        ["core.promo"] = {},
+        ["core.esupports.indent"] = {
+          config = {
+            format_on_escape = true,
+          },
+        },
+        ["core.esupports.metagen"] = {
+          config = {
+            timezone = "implicit-local",
+            type = "auto",
+            update_date = true,
+          },
+        },
         ["core.upgrade"] = {},
         ["core.tangle"] = {},
         ["core.qol.toc"] = {},
