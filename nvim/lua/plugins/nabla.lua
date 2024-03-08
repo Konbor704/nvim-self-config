3055356889c28e58f6fa4c6764a6e6a20b67ed4c
Konbor704 <konbor12@gmail.com>
2024-03-08 21:47:52 +0100

    nvim from kickstart

--- nvim/lua/plugins/nabla.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"jbyuki/nabla.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	build = function()
-		vim.cmd("TSInstall latex")
-	end,
-	lazy = true,
-	config = function() end,
-}
